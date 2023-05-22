035850f1eec4319758d8ee229f7eec16fe2b39ad
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-22 21:04:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,13 +28,13 @@ function App() {
       <LocationProvider>
         <RoutesWithInAnimation/>
       </LocationProvider>
-      {/* <Routes>
+      <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/service' element={<Service/>}/>
         <Route path='/team' element={<Team/>}/>
         <Route path='/contact' element={<Contact/>}/>
-      </Routes> */}
+      </Routes> 
       </AnimatePresence>
       </BrowserRouter>
     </div>
